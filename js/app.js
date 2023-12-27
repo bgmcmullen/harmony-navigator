77dd53df5006081d321a32c1c64287e6e96a78b0
Brendan <bgmcmullen@comcast.net>
2023-12-27 00:46:42 -0800

    added all intervals

--- js/app.js
+++ js/app.js
@@ -1,25 +1,46 @@
 'use strict';
 
-const majorThird = new Interval('M3', ['M3-1.mp3', 'M3-2.mp3'], ['M3-1.png', 'M3-2.png']);
+const minorSecond = new Interval('m2', 'minor2', 'minor2');
 
-const perfectFifth = new Interval('p5', ['p5-1.mp3', 'p5-2.mp3'], ['p5-1.png', 'p5-2.png']);
+const majorSecond = new Interval('M2', 'minor2', 'minor2');
+
+const minorThird = new Interval('m3', 'minor3', 'minor3');
+
+const majorThird = new Interval('M3', 'major3', 'major3');
+
+const perfectFourth = new Interval('p4', 'perfect4', 'perfect4');
+
+const tritone = new Interval('tritone', 'tritone', 'tritone');
+
+const perfectFifth = new Interval('p5', 'perfect5', 'perfect5');
+
+const minorSixth = new Interval('m6', 'minor6', 'minor6');
+
+const majorSixth = new Interval('M6', 'major6', 'major6');
+
+const minorSeventh = new Interval('m7', 'minor7', 'minor7');
+
+const majorSeventh = new Interval('M7', 'major7', 'major7');
+
+const octave = new Interval('octave', 'octave', 'octave');
 
 // Global Variables
 let activeIntervals = [];
-let allIntervals = [majorThird, perfectFifth];
+let allIntervals = [minorSecond, majorSecond, minorThird, majorThird, perfectFourth, tritone, perfectFifth, minorSixth, majorSixth, minorSeventh, majorSeventh, octave];
 
 // DOM Windows
 const staffImage = document.getElementById('staff-image');
 const selectButtons = document.getElementById('select-button-container');
+const nextButton = document.getElementById("next-button");
 const buttons = document.getElementById('button-container');
 const audioElement = document.getElementById('interval-audio');
 const audioSource = document.getElementById('audio-source');
 
 // Create interval objects
-function Interval(type, audioFileArray, imageArray){
+function Interval(type, audioFile, image){
   this.type = type;
-  this.audioFileArray = audioFileArray;
-  this.imageArray = imageArray;
+  this.audioFile = audioFile;
+  this.image = image;
   this.attempts = 0;
   this.successes = 0;
 }
@@ -44,10 +65,11 @@ let intervalObject
 function setInterval(){
   if(intervalArray.length === 0)
     return;
+  staffImage.src = '';
   intervalIndex = Math.floor(Math.random() * intervalArray.length);
-  fileIndex = Math.floor(Math.random() * 2);
+  fileIndex = Math.floor(Math.random() * 10) + 1;
   intervalObject = intervalArray[intervalIndex];
-  audioSource.src = `audio-files/${intervalObject.audioFileArray[fileIndex]}`
+  audioSource.src = `audio-files/${intervalObject.audioFile}-${fileIndex}.mp3`
   audioElement.load();
   intervalObject.attempts++
 }
@@ -56,6 +78,7 @@ setInterval()
 
 buttons.addEventListener('click', handleButtonClick);
 selectButtons.addEventListener('click', handleSelectButtonClick);
+nextButton.addEventListener('click', setInterval);
 
 function handleSelectButtonClick(event){
   if(event.target.id === "select-button-container")
@@ -81,7 +104,7 @@ function handleButtonClick(event){
     return;
   else 
     alert(`wrong ${intervalObject.type}: ${intervalObject.successes} / ${intervalObject.attempts} ${(intervalObject.successes / intervalObject.attempts) * 100}%`);
-  staffImage.src = `img/${intervalObject.imageArray[fileIndex]}`;
-  setInterval()
+  staffImage.src = `img/${intervalObject.image}-${fileIndex}.png`
 }
+
 
